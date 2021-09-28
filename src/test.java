04f3b9eb0470911fed6d29c054df52cb21d4d3d2
Stanley77-web <timothystanleysetiawan555@gmail.com>
2021-09-29 00:42:50 +0700

    Istri udah ngajakin terus

--- src/test.java
+++ src/test.java
@@ -3,15 +3,16 @@ public class test {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        /*
+        
         BacaTulisMatrix bacaMat = new BacaTulisMatrix();
         float[][] matrix;
-        bacaMat.setMat(3,4);
+        bacaMat.setMat(3,3);
         matrix = bacaMat.baca(sc);
         BacaTulisMatrix.tulis(matrix);
         System.out.println();
-        matrix = Gauss.jordan(matrix);
+        matrix = InverswithAdjoin.InversAdjoin(matrix, determinanCramer.detKofaktor(matrix));
         BacaTulisMatrix.tulis(matrix);
+        /*
         float[][] matrix = new float[n][2];
         matrix = BacaInterpolasi.baca(sc,n);
         BacaTulisMatrix.tulis(matrix);
@@ -20,11 +21,13 @@ public class test {
         BacaTulisMatrix.tulis(mHasil);
         int n;
         n = sc.nextInt();*/
+        /*
         float[][] matrix;
         matrix = BacaRegresi.baca(sc);
         BacaTulisMatrix.tulis(matrix);
         System.out.println();
         float[][] mHasil = BacaRegresi.convertKali(matrix);
         BacaTulisMatrix.tulis(mHasil);
+        */
     }
 }
