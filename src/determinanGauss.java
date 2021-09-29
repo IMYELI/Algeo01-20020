fb4109ebb9f8fa5071d62d938fae15c5d7bf040e
wmk567 <16520402@std.stei.itb.ac.id>
2021-09-29 15:31:27 +0700

    determinanGauss buat nanti ditest

---
+++ src/determinanGauss.java
@@ -0,0 +1,51 @@
+public class determinanGauss {
+    public static float detGauss (float matrix[][]){
+        int i,j,k,l,m,cont,same,iszero;
+        float temparr[][];
+        float result,temp;
+
+        temparr = sortMatrixRow.sortMatrix(matrix);
+        result = 1;
+        cont = 0;
+        same = isRowSame.isSame(matrix);
+        
+
+        for (m=0; m<matrix.length; m++){
+            if (temparr[matrix.length-1][m] != 0){
+                cont = 1;
+            }
+        }
+        
+        if (cont == 0 || same == 1){
+            result = 0;
+        }
+
+        else if (cont == 1 && same == 0){
+            for (k=0; k<(matrix.length); k++){
+                for (i=k+1; i<matrix.length; i++){
+                    temp = matrix[i][k];
+                    for (j=k; j<matrix.length; j++){
+                        iszero = isLineZero.isZero(matrix);
+                        if (iszero == 1){
+                            result = 0;
+                            return result;
+                        } 
+                        else {
+                            temparr[i][j] = temparr[i][j] - (matrix[k][j] * temp / matrix[k][k]);
+                        }
+                    }
+                }
+            }
+
+            for(l=0; l<matrix.length; l++){
+                result = result * matrix[l][l];
+            }
+        }
+
+        if (result == -0.0){
+            result = 0;
+        }
+        return result;
+        
+    }
+}
