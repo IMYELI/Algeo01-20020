6a2fd3cea87dfea11c3417472011dc117988e6bd
IMYELI <16520192@std.stei.itb.ac.id>
2021-09-30 22:54:39 +0700

    Gauss tadi sakit sekarang sehat

--- src/Gauss.java
+++ src/Gauss.java
@@ -29,26 +29,30 @@ public class Gauss {
         for(j=0;j<matrix.length;j++){
             copyM = sortRowMatrix(copyM, j); //Mengesort idx bukan nol pertama terurut membesar
             k = BacaTulisMatrix.idxNot0(copyM, j);
-            for(m=k;m<matrix.length;m++){
-                tag = copyM[m][k];              //Pembagi untuk setiap baris
-                for(n=k;n<copyM[0].length;n++){     
-                    if(tag!=0){                 //Mencegah pembagian dengan angka 0
-                        copyM[m][n] = copyM[m][n]/tag;  //Pembagian agar angka paling kiri bernilai 1
-                    } 
+            if(k<matrix[0].length){
+                for(m=k;m<matrix.length;m++){
+                    tag = copyM[m][k];              //Pembagi untuk setiap baris
+                    for(n=k;n<copyM[0].length;n++){     
+                        if(tag!=0){                 //Mencegah pembagian dengan angka 0
+                            copyM[m][n] = copyM[m][n]/tag;  //Pembagian agar angka paling kiri bernilai 1
+                        } 
+                    }
+                }
+
+                for(i=k;i<matrix.length-1;i++){
+                    lock = false;
+                    for(o=k;o<copyM[0].length;o++){
+                        if(copyM[i+1][k] != 0 && o==k){
+                            lock = true;               //Istri minta gembok
+                        }
+                        if (lock){
+                            copyM[i+1][o] = copyM[i+1][o] - copyM[k][o];  //Pengurangan dengan baris paling atas yang bernilai 1
+                        }
+                    }
                 }
             }
 
-            for(i=k;i<matrix.length-1;i++){
-                lock = false;
-                for(o=k;o<copyM[0].length;o++){
-                    if(copyM[i+1][k] != 0 && o==k){
-                        lock = true;               //Istri minta gembok
-                    }
-                    if (lock){
-                        copyM[i+1][o] = copyM[i+1][o] - copyM[k][o];  //Pengurangan dengan baris paling atas yang bernilai 1
-                    }
-                }
-            }
+            
         }
         return copyM;
     } 
@@ -57,11 +61,12 @@ public class Gauss {
         int i,j,k,o;
         double tag;
         //PENERAPAN METODE GAUSS-JORDAN
-        k=matrix.length-1;
+        k=matrix[0].length-1;
         for(j=matrix[0].length-1;j>0;j--){
             for(i=k;i>0;i--){
                 tag = copyM[i-1][k];            //Pengali untuk setiap baris
                 for(o=k;o<copyM[0].length;o++){
+                    
                     if(copyM[k][k] != 0){       //Mencegah pengurangan baris yang depannya sudah 0
                         copyM[i-1][o] = copyM[i-1][o] - (copyM[k][o]*tag);      //Pengurangan dengan baris paling bawah yang bernilai 1
                     //System.out.printf("(%d,%d,%d)\n",i,o,k);
